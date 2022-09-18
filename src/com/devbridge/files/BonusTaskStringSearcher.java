dcb44b0421371d2c392f3fb678de913bf9f34482
Vladas Tiskus <vl.tiskus@gmail.com>
2022-09-18 12:58:51 +0300

    [WIP] Created BonusTaskStringSearcher which handles reading and finding required string in file

---
+++ src/com/devbridge/files/BonusTaskStringSearcher.java
@@ -0,0 +1,13 @@
+package com.devbridge.files;
+
+
+/**
+ * class that handles file reading,
+ * and searching for particular matches in that file's strings.
+ */
+
+public class BonusTaskStringSearcher {
+
+
+
+}
