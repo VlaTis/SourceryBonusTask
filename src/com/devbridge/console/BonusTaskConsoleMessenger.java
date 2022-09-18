b36042490a6e383570d1712c407958ef3185bf86
Vladas Tiskus <vl.tiskus@gmail.com>
2022-09-18 13:15:40 +0300

    [4] updated BonusTaskConsoleMessenger method printWelcome

--- src/com/devbridge/console/BonusTaskConsoleMessenger.java
+++ src/com/devbridge/console/BonusTaskConsoleMessenger.java
@@ -1,12 +1,17 @@
 package com.devbridge.console;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
-/** Class that prints output to console */
+/**
+ * Class that prints output to console
+ */
 
 public class BonusTaskConsoleMessenger {
 
-    /** method that prints List<String> in needed format */
+    /**
+     * method that prints List<String> in needed format
+     */
     public static void print(List<String> someList) {
         if (someList.isEmpty()) {
             System.out.println("No records/elements found!");
@@ -17,8 +22,18 @@ public class BonusTaskConsoleMessenger {
         }
     }
 
+    /**
+     * method that prints hardcoded welcome message
+     */
 
-    public static void printWelcome() {
-        System.out.println("-===== Bonus Task App ======-");
+    public static void printWelcome(String filePathName, Pattern pattern, int comparableInt) {
+        System.out.println(
+                "-===== Bonus Task App ======- \n\n" +
+                        "Current settings: \n" +
+                        "-- Full file path and name => " + filePathName + "\n"
+                        + "-- Regex pattern => " + pattern.toString() + "\n"
+                        + "-- Comparable Number => " + comparableInt + "\n"
+                        + "----------------------------\n" +
+                        "Output of Matches: \n");
     }
 }
